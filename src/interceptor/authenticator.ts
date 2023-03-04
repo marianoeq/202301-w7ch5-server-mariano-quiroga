00afaffc9c28570463224b41cf361a1649977cf5
mariano quiroga <mariano.eq@gmail.com>
2023-03-04 18:42:08 +0100

    Add authenticator interceptor

---
+++ src/interceptor/authenticator.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { Auth, PayloadToken } from '../helpers/auth';
+
+export interface RequestPlus extends Request {
+  dataPlus?: PayloadToken;
+}
+
+export async function authenticator(
+  req: RequestPlus,
+  _res: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = await req.get('Authorization');
+    if (!authHeader) throw new Error('Invalid Token');
+    if (!authHeader.startsWith('Bearer')) throw new Error('Invalid Token');
+    const token = authHeader.slice(7);
+    const payload = Auth.verifyJWT(token);
+    req.dataPlus = payload;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
