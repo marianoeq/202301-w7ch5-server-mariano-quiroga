08d37a0c9afb205630ca009c042e064396cddfec
mariano quiroga <mariano.eq@gmail.com>
2023-03-04 11:18:58 +0100

    Add database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import http from 'http';
 import createDebug from 'debug';
 import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
 
 const debug = createDebug('W7:index');
 const PORT = process.env.PORT || 3232;
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB: ', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    server.emit('error' + error);
+  });
 
 server.on('listening', () => {
   debug('Server listening on PORT ' + PORT);
