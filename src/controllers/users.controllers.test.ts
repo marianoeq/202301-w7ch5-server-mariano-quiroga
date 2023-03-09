e49f2c4d5c5f43ee5b53cd7d1b526bdf4802eb50
mariano quiroga <mariano.eq@gmail.com>
2023-03-10 00:22:35 +0100

    working on login test

--- src/controllers/users.controllers.test.ts
+++ src/controllers/users.controllers.test.ts
@@ -2,6 +2,7 @@ import { UsersController } from './users.controller';
 import { NextFunction, Request, Response } from 'express';
 import { Repo } from '../repository/repo.interface';
 import { User } from '../entities/user.model';
+import { Auth, PayloadToken } from '../helpers/auth';
 
 jest.mock('../helpers/auth.js');
 
@@ -44,7 +45,7 @@ describe('Given the UsersController class ', () => {
       expect(next).toHaveBeenCalled();
     });
   });
-  describe('When getUserName is called ', () => {
+  describe('When getUserByName is called ', () => {
     test('Then if no name was passed it should throw an Error and next function catch the error', async () => {
       const req = { params: { email: '' } } as unknown as Request;
       await controller.getUserByName(req, res, next);
@@ -62,6 +63,78 @@ describe('Given the UsersController class ', () => {
       const req = { params: { name: 'test' } } as unknown as Request;
       (MockRepo.search as jest.Mock).mockResolvedValue(['test']);
       await controller.getUserByName(req, res, next);
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+  describe('When login is called ', () => {
+    test('Then if no email was passed it should throw an Error and next function catch it', async () => {
+      const req = {
+        body: {
+          password: 'test',
+        },
+      } as unknown as Request;
+      await controller.login(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if no password was passed it should throw an Error and next function catch it', async () => {
+      const req = {
+        body: {
+          email: 'test',
+        },
+      } as unknown as Request;
+      await controller.login(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if search return an empty array it should return an error and next be called', async () => {
+      const req = {
+        body: { email: 'test', password: 'test' },
+      } as unknown as Request;
+
+      (MockRepo.search as jest.Mock).mockResolvedValue([]);
+      await controller.login(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if Auth.compare is false it should return an error and next be called', async () => {
+      const req = {
+        body: { email: 'test', password: 'test' },
+      } as unknown as Request;
+      (MockRepo.search as jest.Mock).mockResolvedValue(['test']);
+      (Auth.compare as jest.Mock).mockResolvedValue(false);
+      await controller.login(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When register is called ', () => {
+    test('Then if no email was passed it should throw an Error and next function catch it', async () => {
+      const req = {
+        body: {
+          password: 'test',
+        },
+      } as unknown as Request;
+      await controller.register(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if no password was passed it should throw an Error and next function catch it', async () => {
+      const req = {
+        body: {
+          email: 'test',
+        },
+      } as unknown as Request;
+      await controller.register(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test('Then if data was passed correctly it should return a json response', async () => {
+      const req = {
+        body: { email: 'test', password: 'test' },
+      } as unknown as Request;
+
+      await controller.register(req, res, next);
+      expect(MockRepo.create).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalled();
     });
   });
